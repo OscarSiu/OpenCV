ab69d69e8d189258f36c6bf4faebcac484b0ae62
Siu Cheuk Yin <68896149+OscarSiu@users.noreply.github.com>
2023-07-25 14:39:39 +0800

    Update 3_VideoCapure.py

--- 3_VideoCapure.py
+++ 3_VideoCapure.py
@@ -1,12 +1,14 @@
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
    print("Cannot open camera")
    exit()
    
-   
+fps = cap.get(cv2.CAP_PROP_FPS)
+print("Frame rate: ", int(fps), "FPS")
+
 # Set frame dimensions
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
